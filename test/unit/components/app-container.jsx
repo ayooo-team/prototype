32904f65027bcbfdccecd2c5e3afaecd717768a6
Mina <wilhelmina.jane@gmail.com>
2016-03-24 18:13:02 +0000

    simple test

--- test/unit/components/app-container.jsx
+++ test/unit/components/app-container.jsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import tape from 'tape';
+
 import { scryRenderedDOMComponentsWithTag } from 'react-addons-test-utils';
 import { shallow, mount } from 'enzyme';
 
@@ -10,7 +11,11 @@ import AppContainer from '../../../src/js/components/app-container.jsx';
 
 tape('AppContainer exists', (t) => {
 
-    
+    const rendered = mount(<AppContainer />);
+
+
+    console.log(rendered.props());
+    // t.equal(text, "Hey", 'They are the same');
 
     t.end();
 });
