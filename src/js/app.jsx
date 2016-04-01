7e5ccecaa684c81c73fccca92bd1789059ddce6a
Marie <marie.kasai@live.co.uk>
2016-04-01 18:44:48 +0100

    adds new routes to app.jsx

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -13,7 +13,9 @@ import Traveller from './components/traveller/journey.jsx';
 import UserDetails from './components/save-user-details.jsx';
 import Space from './components/traveller/luggage-allowance.jsx';
 import ConfirmTravel from './components/traveller/confirm-travel.jsx';
-import Admin        from './components/admin.jsx';
+import PriceSuggestion from './components/price.jsx';
+import TravelPost from './components/traveller/travel-post.jsx';
+import Admin from './components/admin.jsx';
 
 const routes = (
     <Route path="/" component={ AppContainer } >
@@ -24,6 +26,8 @@ const routes = (
         <Route path="/journey" component={ Traveller } />
         <Route path="/luggage-allowance" component={ Space } />
         <Route path="/confirm-travel" component={ ConfirmTravel } />
+        <Route path="/price" component={ PriceSuggestion } />
+        <Route path="/travel-post" component={ TravelPost } />
         <Route path="/admin" component={ Admin } />
     </Route>
 );
