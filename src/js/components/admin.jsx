98cce38363b60fce075dc58b5d136f2a6caaf351
Mina <wilhelmina.jane@gmail.com>
2016-04-01 16:37:41 +0100

    adds to admin page

--- src/js/components/admin.jsx
+++ src/js/components/admin.jsx
@@ -10,6 +10,8 @@ class Admin extends React.Component {
         return (
             <div className="page">
 
+                <h1>App admin</h1><br/>
+                <h3> Choose a file to download: </h3>
                 <DownloadButton query="?filename=senders" filename="senders.csv" buttonText="Sender requests" />
                 <DownloadButton query="?filename=travellers" filename="travellers.csv" buttonText="Traveller requests" />
             </div>
