0689d22c2a2440589f986d8ad8069f83b0a9c683
Marie Kasai <marie.kasai@live.co.uk>
2016-04-03 21:33:47 +0100

    gets radio button information and stores in parent state

--- src/js/components/traveller/luggage-allowance.jsx
+++ src/js/components/traveller/luggage-allowance.jsx
@@ -8,23 +8,48 @@ import GhostButton from '../ghost-button.jsx';
 class Space extends React.Component {
 
     constructor() {
-      super();
 
-      this.saveDataToParentState = this.saveDataToParentState.bind(this);
+        super();
+        this.storeRadioOption = this.storeRadioOption.bind(this);
+        this.radioOptionClicked = this.radioOptionClicked.bind(this);
+        this.saveDataToParentState = this.saveDataToParentState.bind(this);
+    }
+
+    radioOptionClicked() {
+
+        this.state && this.state.chosenOption ?
+          this.refs.weight.value ? (
+            this.saveDataToParentState(), window.location="/#travel-post/price"
+          ) : (
+            alert("Please tell us your maximum weight allowance")
+          ) :
+        alert("Please tell us the capacity you have in your luggage");
+    }
+
+    storeRadioOption (event) {
+
+        event.preventDefault();
+        this.setState({
+            chosenOption: event.target.id,
+            showTick: "block",
+            hideTick: "none"
+        });
     }
 
     saveDataToParentState() {
 
         const saveLuggageData = this.props.saveLuggageData;
-        saveLuggageData("LUGGAGE");
+        saveLuggageData({
+            chosenOption: this.state.chosenOption,
+            weightAllowance: this.refs.weight.value
+        });
     }
-
 
     render () {
 
         var radioDiv = data["space"].map( (element, index, array) => {
 
-            return <Options key={ index } blockTitle={ element.title } blockImage={ element.image } blockText={ element.text } />
+            return <Options onClick={ this.storeRadioOption } key={ index } blockTitle={ element.title } blockImage={ element.image } blockText={ element.text } />
         });
 
         return (
@@ -34,7 +59,6 @@ class Space extends React.Component {
                 <div className="form-block">
                     <h3 className="form-block-title">DEFINE YOUR CAPACITY</h3>
                     { radioDiv }
-
                 </div>
 
                 <div className="form-block">
@@ -43,7 +67,8 @@ class Space extends React.Component {
                     <span className="flex-item" type="text">Kg</span>
                 </div>
 
-                <GhostButton onClick={ this.saveDataToParentState } pageLink="/travel-post/price" buttonText="NEXT" />
+                  <GhostButton onClick={ this.radioOptionClicked } buttonText="NEXT" />
+
             </div>
         )
     }
