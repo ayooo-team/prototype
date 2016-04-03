7f7831225a5071274670d6788b0bfe8df157cecd
Mina <wilhelmina.jane@gmail.com>
2016-04-03 15:20:00 +0100

    styles options component

--- src/js/components/options.jsx
+++ src/js/components/options.jsx
@@ -7,10 +7,12 @@ class Options extends React.Component {
     render () {
 
         return (
-            <div className="radio-block">
-                <img className={ "radio-block-image radio-block-image-" + this.props.blockTitle } src={ this.props.blockImage } />
-                <h4 className="radio-block-title">{ this.props.blockTitle }</h4>
-                <p className="radio-block-text">{ this.props.blockText }</p>
+            <div className="radio-block flex-wrapper">
+                <img className={ "radio-block-image flex-item radio-block-image-" + this.props.blockTitle } src={ this.props.blockImage } />
+                <div className="description flex-item">
+                    <h4 className="radio-block-title">{ this.props.blockTitle }</h4>
+                    <p className="radio-block-ttle">{ this.props.blockText }</p>
+                </div>
             </div>
         );
     }
