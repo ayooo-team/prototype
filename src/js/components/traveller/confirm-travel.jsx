1b2f6267cb123c3e790a9ed4b6db2e3f3ea2c6eb
Marie Kasai <marie.kasai@live.co.uk>
2016-04-03 16:15:28 +0100

    adds modal base for confirmation page

--- src/js/components/traveller/confirm-travel.jsx
+++ src/js/components/traveller/confirm-travel.jsx
@@ -5,12 +5,24 @@ import GhostButton from '../ghost-button.jsx';
 
 class ConfirmTravel extends React.Component {
 
+    checkAllInputFieldsComplete () {
+
+        console.log('input fields complete ? submitToDB: no');
+    }
+
     render () {
 
         return (
-            <div className="page">
-                <h1>CONFIRM TRAVEL</h1>
-            </div>
+
+              <div className="page form">
+
+                  <div className="form-block">
+                      <label className="form-block-title">DEPARTURE LOCATION:</label>
+                      <span className="form-input-data"></span>
+                  </div>
+
+                  <GhostButton onClick={this.checkAllInputFieldsComplete} buttonText="CONFIRM" />
+              </div>
         )
     }
 }
