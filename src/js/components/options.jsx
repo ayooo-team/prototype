997a747bd274d968d4eb4e0138cf60ed03960034
Mina <wilhelmina.jane@gmail.com>
2016-04-03 17:09:44 +0100

    adds more radio block styling.  removes unnecessary global css rules

--- src/js/components/options.jsx
+++ src/js/components/options.jsx
@@ -7,8 +7,10 @@ class Options extends React.Component {
     render () {
 
         return (
-            <div className="radio-block flex-wrapper">
-                <img className={ "radio-block-image flex-item radio-block-image-" + this.props.blockTitle } src={ this.props.blockImage } />
+            <div className="radio-block flex-container">
+                <div className="radio-block-image-wrapper flex-item">
+                    <img className={ "radio-block-image radio-block-image-" + this.props.blockTitle } src={ this.props.blockImage } />
+                </div>
                 <div className="description flex-item">
                     <h4 className="radio-block-title">{ this.props.blockTitle }</h4>
                     <p className="radio-block-ttle">{ this.props.blockText }</p>
