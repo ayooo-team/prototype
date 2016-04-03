5e6568b4573c3647681264f1ac069e20029b0d32
Marie Kasai <marie.kasai@live.co.uk>
2016-04-04 00:41:42 +0100

    makes luggage-allowance communal between sender and traveller, and creates components to fulfil wireframe views

---
+++ src/js/components/sender/confirm-parcel.jsx
@@ -0,0 +1,75 @@
+'use strict';
+
+import React from 'react';
+import GhostButton from '../ghost-button.jsx';
+
+class ConfirmParcel extends React.Component {
+
+    constructor(props) {
+
+        super();
+        this.confirmPost = this.confirmPost.bind(this);
+
+        //CHECK IF USER PROFILE EXISTS ON FIREBASE --> if EXISTS, direct to alert box, else --> populate modal
+    }
+
+    confirmPost () {
+
+        //MAKE AJAX REQUEST TO SAVE DATA TO ESDB
+        alert("Thank you! AYOOO will be in touch soon!");
+    }
+
+    render () {
+
+        return (
+
+              <div className="page form">
+
+                  <div className="form-block">
+                      <label className="form-block-title">FROM:</label>
+                      <p className="form-input-data">
+                      { "City: " + this.props.pickUpData.fromCity }
+                      </p>
+                      <p className="form-input-data">
+                      { "PostCode: " + this.props.pickUpData.fromPostCode }
+                      </p>
+                      <p className="form-input-data">
+                      { "Person Picking Up: " + this.props.pickUpData.pickUpIdentity }
+                      </p>
+                  </div>
+
+                  <div className="form-block">
+                      <label className="form-block-title">TO:</label>
+                      <p className="form-input-data">
+                      { "City: " + this.props.pickUpData.toCity }
+                      </p>
+                      <p className="form-input-data">
+                      { "PostCode: " + this.props.pickUpData.toPostCode }
+                      </p>
+                      <p className="form-input-data">
+                      { "Person Recieving: " + this.props.pickUpData.recipientIdentity }
+                      </p>
+                  </div>
+
+                  <div className="form-block">
+                      <label className="form-block-title">PARCEL SIZE:</label>
+                      <p className="form-input-data">{
+                          "Size: " + this.props.parcelSize.chosenOption
+                      } </p>
+                  </div>
+
+                  <div className="form-block">
+                      <label className="form-block-title">ASKING PRICE:</label>
+                      <p className="form-input-data">{
+                          "Price(£): " + this.props.priceData.price
+                      } </p>
+                  </div>
+
+                  <GhostButton onClick={this.confirmPost} buttonText="CONFIRM" />
+
+              </div>
+        )
+    }
+}
+
+export default ConfirmParcel
---
+++ src/js/components/sender/send-post.jsx
@@ -0,0 +1,60 @@
+'use strict';
+
+import React from 'react';
+
+class SendPost extends React.Component {
+
+    constructor (props) {
+        super();
+
+        this.state = {
+          type: "send",
+          pageType: "/#send-post",
+          pricePageDescription: "What is the maximum price you are willing to pay?",
+          pickUpData: "default",
+          parcelSize: "default",
+          requestedDate: "default",
+          priceData: "default"
+        }
+
+        this.createComponentDataSaverFor = this.createComponentDataSaverFor.bind(this);
+        this.attachActionsTo = this.attachActionsTo.bind(this);
+    }
+
+    createComponentDataSaverFor (formName) {
+
+        return (formData) => {
+
+          this.setState({
+              [formName]: formData
+          })
+        }
+    }
+
+    attachActionsTo (component) {
+
+        return React.cloneElement(component, {
+
+          type: this.state.type,
+          pageType: this.state.pageType,
+          pricePageDescription: this.state.pricePageDescription,
+          savePickUpData: this.createComponentDataSaverFor("pickUpData"),
+          saveParcelSize: this.createComponentDataSaverFor("parcelSize"),
+          saveRequestedDate: this.createComponentDataSaverFor("requestedDate"),
+          savePriceData: this.createComponentDataSaverFor("priceData"),
+          pickUpData: this.state.pickUpData,
+          parcelSize: this.state.parcelSize,
+          requestedDateData: this.state.requestedDateData,
+          priceData: this.state.priceData
+        });
+    }
+
+    render () {
+
+        return (
+            this.attachActionsTo(this.props.children)
+        );
+    }
+}
+
+export default SendPost
