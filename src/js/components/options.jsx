d562f59dc00f442caa5effe0f7e8723c3193c479
Mina <wilhelmina.jane@gmail.com>
2016-04-04 12:51:34 +0100

    implement grid system

--- src/js/components/options.jsx
+++ src/js/components/options.jsx
@@ -13,18 +13,18 @@ class Options extends React.Component {
 
         return (
 
-            <div className="radio-row" id={ this.props.blockTitle } onClick={ this.props.onClick }>
+            <div className="radio-row row" id={ this.props.blockTitle } onClick={ this.props.onClick }>
 
-                <div className="radio-image-wrapper">
+                <div className="radio-image-wrapper col-2">
                     <img className={ "radio-image radio-block-image-" + this.props.blockTitle } src={ this.props.blockImage } />
                 </div>
 
-                <div className="description">
+                <div className="description col-9">
                     <h4 className="radio-block-title" id={ this.props.blockTitle }>{ this.props.blockTitle }</h4>
                     <p className="radio-block-text" id={ this.props.blockTitle }>{ this.props.blockText }</p>
                 </div>
 
-                <img className="radio-selected-tick" id={ this.props.blockTitle } src="https://cloud.githubusercontent.com/assets/13470325/14233556/3e0f5e2c-f9c4-11e5-8337-17c162b678a1.png" />
+                <img className="radio-selected-tick col-1" id={ this.props.blockTitle } src="https://cloud.githubusercontent.com/assets/13470325/14233556/3e0f5e2c-f9c4-11e5-8337-17c162b678a1.png" />
             </div>
         );
     }
