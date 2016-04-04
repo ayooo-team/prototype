31726b972db5a1b0abc9c73e23086bfbb17c86df
Marie Kasai <marie.kasai@live.co.uk>
2016-04-04 12:31:59 +0100

    adds set delivery date component

---
+++ src/js/components/sender/set-delivery-date.jsx
@@ -0,0 +1,19 @@
+'use strict';
+
+import React from 'react';
+
+class SetDeliveryDate extends React.Component {
+
+    render () {
+
+        return (
+
+            <div>
+                SET DELIVERY DATE
+            </div>
+        )
+    }
+
+}
+
+export default SetDeliveryDate
