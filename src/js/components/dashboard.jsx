06ef0c370ff19b32a802e375dc90e265c4307bc3
Mina <wilhelmina.jane@gmail.com>
2016-04-09 17:22:28 +0100

    fully styles Traveller user flow

--- src/js/components/dashboard.jsx
+++ src/js/components/dashboard.jsx
@@ -25,7 +25,7 @@ class Dashboard extends React.Component {
         return (
 
             <div className="page dashboard-container">
-                <img className="logo" src="/logo.png" />
+                <img className="logo" src="img/logo.png" />
                 <h1>AYOOO</h1>
                 <h4>How are you using the app today?</h4>
                 <div className="button-wrapper">
