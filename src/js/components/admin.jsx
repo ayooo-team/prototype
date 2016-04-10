32d766d6b59d1a61158b87407fd9ac6a91589da8
Marie <marie.kasai@live.co.uk>
2016-04-10 17:24:50 +0100

    WIP admin login

--- src/js/components/admin.jsx
+++ src/js/components/admin.jsx
@@ -8,18 +8,21 @@ class Admin extends React.Component {
 
     constructor (props) {
 
-        super();
+        super(props);
+
         this.getUserID = this.getUserID.bind(this);
         this.isUserAuthenticated = this.isUserAuthenticated.bind(this);
         this.getFormData = this.getFormData.bind(this);
 
-        this.state = { login: "false" }
+        this.state = { loading: false }
 
         this.getUserID((response) => {
             response ?
             this.isUserAuthenticated(response, (isAdmin) => {
-                isAdmin === 'admin' ? this.setState({ login: response }) : console.log(response);
-            }) : console.log(response);
+                isAdmin === 'admin' ?
+                this.setState({ loading: true, userKnown: true, login: response }) :
+                this.setState({ loading: true, userKnown: false, login: false })
+            }) : console.log("2", "NO ID"), this.state = { loading: true };
 
         });
     }
@@ -51,19 +54,61 @@ class Admin extends React.Component {
         });
     }
 
+    getFormData (event) {
+
+        event.preventDefault();
+
+        const credentials = {
+            email: this.refs.email.value,
+            password: this.refs.password.value
+        };
+
+        this.logUserIn(credentials);
+    }
+
+    logUserIn (credentials) {
+
+        const firebaseApp = new Firebase("https://ayooo.firebaseio.com/");
+        firebaseApp.authWithPassword(credentials, (error, authData) => {
+
+        });
+    }
+
     render () {
 
-        return this.state.login === process.env.ADMIN ? (
+        return this.state && this.state.loading ?
 
-            <div className="page">
+        this.state.userKnown ?
 
-                <h1 className="admin-header flex-item">App admin</h1><br/>
-                <h3 className="flex-item"> Choose a file to download: </h3>
-                <div className="download-button-wrapper flex-item">
-                    <DownloadButton query="?filename=senders" filename="senders.csv" buttonText="Sender requests" />
-                    <DownloadButton query="?filename=travellers" filename="travellers.csv" buttonText="Traveller requests" />
+            this.state.login === '1d662c56-9470-4704-8ea6-5ea27f55ee98' ? (
+
+                <div className="page">
+
+                    <h1 className="admin-header flex-item">App admin</h1><br/>
+                    <h3 className="flex-item"> Choose a file to download: </h3>
+                    <div className="download-button-wrapper flex-item">
+                        <DownloadButton query="?filename=senders" filename="senders.csv" buttonText="Sender requests" />
+                        <DownloadButton query="?filename=travellers" filename="travellers.csv" buttonText="Traveller requests" />
+                    </div>
                 </div>
-            </div>
+            ) : (
+                <div className="page form">
+
+                    <h1 className="login-title">Please Log In:</h1>
+
+                    <div className="form-block">
+                        <label className="form-label login-width-adjust">Email:</label>
+                        <input className="form-input" type="text" ref="email" />
+                    </div>
+
+                    <div className="form-block">
+                        <label className="form-label">Password:</label>
+                        <input className="form-input" type="password" ref="password" />
+                    </div>
+
+                    <GhostButton onClick={ this.getFormData } buttonText={ "LOG IN / SIGN UP" } />
+
+                </div>
         ) : (
             <div className="page form">
 
@@ -82,7 +127,12 @@ class Admin extends React.Component {
                 <GhostButton onClick={ this.getFormData } buttonText={ "LOG IN / SIGN UP" } />
 
             </div>
-        );
+        ) : (
+            <div className="page">
+                <h1>LOADING...</h1>
+            </div>
+        )
+
     }
 }
 
