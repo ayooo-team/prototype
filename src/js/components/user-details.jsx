dab23f72f8bd199596bec8091c2173035b5635d1
Mina <wilhelmina.jane@gmail.com>
2016-04-11 12:48:21 +0100

    styles user-details.jsx

--- src/js/components/user-details.jsx
+++ src/js/components/user-details.jsx
@@ -82,56 +82,60 @@ class UserDetails extends React.Component {
         let leftColumn = classnames(
             "form-block-title",
             "label",
-            "col-4"
+            "col-12"
         );
         let rightColumn = classnames(
-            "col-6"
+            "col-12"
         );
 
         return this.state ? (
-            <div className="page">
-                <h3>
-                    It seems we're still missing a few bits of information from you...
-                    To make a post, please fill in the empty fields.
-                </h3>
+            <div className="user-details-page data-collection-page">
 
                 <h1>USER DETAILS</h1>
-
+                <p>
+                    It seems we're still missing a few bits of information from you...
+                    To make a post, please fill in the empty fields.
+                </p>
                 <div>
                     <h4 className={ leftColumn }>Name</h4>
                     <input className={ rightColumn } type="text" ref="name" defaultValue={ this.state.savedName } />
                 </div>
-                <div className="form-block row">
-                    <h4 className={ leftColumn }>Age</h4>
-                    <input className={ rightColumn } type="text" ref="age" defaultValue={ this.state.savedAge }/>
-                </div>
+                <div className="user-details">
 
-                <div className="form-block row">
-                    <h4 className={ leftColumn }>Profession</h4>
-                    <input className={ rightColumn } type="text" ref="profession" defaultValue={ this.state.savedProfession } />
-                </div>
+                    <div className="form-block row">
+                        <h4 className={ leftColumn }>Age</h4>
+                        <input className={ rightColumn } type="text" ref="age" defaultValue={ this.state.savedAge }/>
+                    </div>
 
-                <div className="form-block row">
-                    <h4 className={ leftColumn }>Nationality</h4>
-                    <input className={ rightColumn } type="text" ref="nationality" defaultValue={ this.state.savedNationality }/>
-                </div>
+                    <div className="form-block row">
+                        <h4 className={ leftColumn }>Profession</h4>
+                        <input className={ rightColumn } type="text" ref="profession" defaultValue={ this.state.savedProfession } />
+                    </div>
 
-                <div className="form-block row">
-                    <h4 className={ leftColumn }>Mobile Number</h4>
-                    <input className={ rightColumn } type="text" ref="mobileNumber" defaultValue={ this.state.savedMobileNumber }/>
+                    <div className="form-block row">
+                        <h4 className={ leftColumn }>Nationality</h4>
+                        <input className={ rightColumn } type="text" ref="nationality" defaultValue={ this.state.savedNationality }/>
+                    </div>
+
+                    <div className="form-block row">
+                        <h4 className={ leftColumn }>Mobile Number</h4>
+                        <input className={ rightColumn } type="text" ref="mobileNumber" defaultValue={ this.state.savedMobileNumber }/>
+                    </div>
                 </div>
 
                 <GhostButton onClick={ this.getFormData.bind(this) } buttonText="Submit" />
             </div>
         ) : (
-            <div className="page data-collection-page">
-                <h3>
+            <div className="user-details-page data-collection-page">
+
+
+                <h1>USER DETAILS</h1>
+
+                <p>
                     To ensure that our community is trustworthy,
                     we record information of every member's identity.
                     To make a post, please tell us who you are.
-                </h3>
-
-                <h1>USER DETAILS</h1>
+                </p>
 
                 <div className="user-details">
                     <div className="form-block row">
@@ -151,7 +155,7 @@ class UserDetails extends React.Component {
 
                     <div className="form-block row">
                         <h4 className={ leftColumn }>Nationality</h4>
-                        <input className={ rightColumn } type="text" ref="mobileNumber"/>
+                        <input className={ rightColumn } type="text" ref="nationality"/>
                     </div>
 
                     <div className="form-block row">
