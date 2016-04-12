a28b61caa855c9d129741246e343fab6510d7510
Marie <marie.kasai@live.co.uk>
2016-04-13 00:33:53 +0100

    removes unused classname

--- src/js/components/parcel-size.jsx
+++ src/js/components/parcel-size.jsx
@@ -122,7 +122,7 @@ class ParcelSize extends React.Component {
 
                   <div className="weight-input-container row">
                       <h4 className="input-title col-7">MAX WEIGHT:</h4>
-                      <input className="input-number large col-5" type="number" ref="weight" />
+                      <input className="input-number" type="number" ref="weight" />
                       <p className="unit col-2">kg</p>
                   </div>
               </div>
